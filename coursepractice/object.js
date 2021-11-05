e243f4cd9ed8f26a209b6a13bc66597dc1cd3d3a
Volodymyr Horbachov <gorbacovvolodimir@gmail.com>
2021-11-05 12:34:02 +0200

    object 05.11.21

--- coursepractice/object.js
+++ coursepractice/object.js
@@ -1,4 +1,49 @@
-let user = {
+const Marvel = {
+    createYear: 1939,
+    mainCharecter: 'Captain America',
+    owner: 'Disney'
+}
+console.log(Marvel.valueOf());
+console.log(Marvel.toString());
+console.log(Marvel.hasOwnProperty('createYear'));
+// Object.freeze(Marvel);
+Marvel.mainVillian = 'StepnWoolf';
+console.log(Marvel);
+console.log(Object.isFrozen(Marvel));
+
+const DC = {
+    createYear: 1934,
+    mainCharecter: 'Superman',
+    owner: 'WB'
+}
+console.log(Object.keys(DC));
+console.log(Object.values(DC));
+console.log(Object.entries(DC));
+
+const newComics = Object.assign({}, DC);
+// console.log(newComics);
+
+const newDC = Object.create(DC);
+console.log(newDC.createYear);
+
+// DC['mainVillian'] = 'DarkSide';
+if (DC.mainVillian === 'DarkSide') {
+    DC.mainTeam = 'Justice League';
+} else {
+    DC.notIntresting = true;
+}
+
+console.log(DC);
+
+Marvel.mainVillian = 'Tanos';
+DC['mainVillian'] = 'DarkSide';
+console.log(Marvel);
+console.log(DC);
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+const user = {
     name: 'Volodymyr',
     age: 32,
     'I like': 'Gym',
@@ -45,7 +90,7 @@ console.log(user['I like']);
 delete user.surname;
 console.log(user);
 
-let obj = (name, age) => {
+const obj = (name, age) => {
     return {
         name,
         age,
